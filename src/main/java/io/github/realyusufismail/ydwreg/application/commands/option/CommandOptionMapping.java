d4329021a57f0cca91f6429b954a2775c79488b6
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-06-01 19:14:28 +0100

    Update CommandOptionMapping.java

--- src/main/java/io/github/realyusufismail/ydwreg/application/commands/option/CommandOptionMapping.java
+++ src/main/java/io/github/realyusufismail/ydwreg/application/commands/option/CommandOptionMapping.java
@@ -3,6 +3,15 @@ package io.github.realyusufismail.ydwreg.application.commands.option;
 import com.fasterxml.jackson.databind.JsonNode;
 import io.github.realyusufismail.ydw.application.commands.option.CommandInteractionDataOption;
 import io.github.realyusufismail.ydw.application.commands.option.OptionType;
+import io.github.realyusufismail.ydw.entities.User;
+import io.github.realyusufismail.ydw.entities.guild.Channel;
+import io.github.realyusufismail.ydw.entities.guild.Member;
+import io.github.realyusufismail.ydw.entities.guild.Role;
+import io.github.realyusufismail.ydw.entities.guild.channel.NewsChannel;
+import io.github.realyusufismail.ydw.entities.guild.channel.TextChannel;
+import io.github.realyusufismail.ydw.entities.guild.channel.VoiceChannel;
+
+import java.util.Optional;
 
 public class CommandOptionMapping implements CommandInteractionDataOption {
 
@@ -18,7 +27,77 @@ public class CommandOptionMapping implements CommandInteractionDataOption {
     }
 
     @Override
+    public OptionType getType() {
+        return null;
+    }
+
+    @Override
     public String getName() {
         return name;
+    }
+
+    @Override
+    public Optional<Boolean> isFocused() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<String> getAsString() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Integer> getAsInt() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Double> getAsDouble() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Long> getAsLong() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Boolean> getAsBoolean() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Member getAsMember() {
+        return null;
+    }
+
+    @Override
+    public User getAsUser() {
+        return null;
+    }
+
+    @Override
+    public Channel getAsChannel() {
+        return null;
+    }
+
+    @Override
+    public TextChannel getAsTextChannel() {
+        return null;
+    }
+
+    @Override
+    public VoiceChannel getAsVoiceChannel() {
+        return null;
+    }
+
+    @Override
+    public NewsChannel getAsNewsChannel() {
+        return null;
+    }
+
+    @Override
+    public Role getAsRole() {
+        return null;
     }
 }
