1cc095abbc66201bfc6246e9fb96b89521e3d098
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-06-06 22:56:45 +0100

    Update Interaction.java

--- src/main/java/io/github/realyusufismail/websocket/handle/handles/interaction/Interaction.java
+++ src/main/java/io/github/realyusufismail/websocket/handle/handles/interaction/Interaction.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import io.github.realyusufismail.websocket.handle.Handle;
 import io.github.realyusufismail.ydw.YDW;
 
-public class InteractionHandler extends Handle {
-    protected InteractionHandler(JsonNode json, YDW ydw) {
+public class Interaction extends Handle {
+    protected Interaction(JsonNode json, YDW ydw) {
         super(json, ydw);
     }
 
