f74055a127573a09294f46e95f764cb1f491024d
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-07-11 11:57:49 +0100

    Removed un needed space

--- src/main/java/io/github/realyusufismail/ydwreg/application/commands/slash/reply/ReplyConfigReg.java
+++ src/main/java/io/github/realyusufismail/ydwreg/application/commands/slash/reply/ReplyConfigReg.java
@@ -15,8 +15,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-            
+ */ 
 package io.github.realyusufismail.ydwreg.application.commands.slash.reply;
 
 import io.github.realyusufismail.ydw.application.commands.reply.ReplyConfig;
--- src/main/java/io/github/realyusufismail/ydwreg/entities/sticker/StickerFormatType.java
+++ src/main/java/io/github/realyusufismail/ydwreg/entities/sticker/StickerFormatType.java
@@ -15,8 +15,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-            
+ */ 
 package io.github.realyusufismail.ydwreg.entities.sticker;
 
 import org.jetbrains.annotations.NotNull;
