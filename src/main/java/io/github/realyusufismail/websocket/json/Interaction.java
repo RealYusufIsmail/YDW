ca6956c9602ab3edba3f6dbf61e4209f3aaeb411
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-07-26 11:44:00 +0100

    spotless

--- src/main/java/io/github/realyusufismail/websocket/json/Interaction.java
+++ src/main/java/io/github/realyusufismail/websocket/json/Interaction.java
@@ -1,5 +1,21 @@
-//YApi QuickType插件生成，具体参考文档:https://github.com/RmondJone/YapiQuickType
-
+/*
+ * Copyright 2022 Yusuf Arfan Ismail and other YDW contributors.
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
 package io.github.realyusufismail.websocket.json;
 
 import java.time.OffsetDateTime;
@@ -11,17 +27,37 @@ public class Interaction {
     private String t;
     private D d;
 
-    public long getOp() { return op; }
-    public void setOp(long value) { this.op = value; }
+    public long getOp() {
+        return op;
+    }
 
-    public long getS() { return s; }
-    public void setS(long value) { this.s = value; }
+    public void setOp(long value) {
+        this.op = value;
+    }
 
-    public String getT() { return t; }
-    public void setT(String value) { this.t = value; }
+    public long getS() {
+        return s;
+    }
 
-    public D getD() { return d; }
-    public void setD(D value) { this.d = value; }
+    public void setS(long value) {
+        this.s = value;
+    }
+
+    public String getT() {
+        return t;
+    }
+
+    public void setT(String value) {
+        this.t = value;
+    }
+
+    public D getD() {
+        return d;
+    }
+
+    public void setD(D value) {
+        this.d = value;
+    }
 
     public static class D {
         private Data data;
@@ -36,38 +72,93 @@ public class Interaction {
         private String token;
         private String guildLocale;
 
-        public Data getData() { return data; }
-        public void setData(Data value) { this.data = value; }
+        public Data getData() {
+            return data;
+        }
 
-        public Member getMember() { return member; }
-        public void setMember(Member value) { this.member = value; }
+        public void setData(Data value) {
+            this.data = value;
+        }
 
-        public String getGuildid() { return guildid; }
-        public void setGuildid(String value) { this.guildid = value; }
+        public Member getMember() {
+            return member;
+        }
 
-        public String getid() { return id; }
-        public void setid(String value) { this.id = value; }
+        public void setMember(Member value) {
+            this.member = value;
+        }
 
-        public long getType() { return type; }
-        public void setType(long value) { this.type = value; }
+        public String getGuildid() {
+            return guildid;
+        }
 
-        public String getLocale() { return locale; }
-        public void setLocale(String value) { this.locale = value; }
+        public void setGuildid(String value) {
+            this.guildid = value;
+        }
 
-        public long getVersion() { return version; }
-        public void setVersion(long value) { this.version = value; }
+        public String getid() {
+            return id;
+        }
 
-        public String getChannelid() { return channelid; }
-        public void setChannelid(String value) { this.channelid = value; }
+        public void setid(String value) {
+            this.id = value;
+        }
 
-        public String getApplicationid() { return applicationid; }
-        public void setApplicationid(String value) { this.applicationid = value; }
+        public long getType() {
+            return type;
+        }
 
-        public String getToken() { return token; }
-        public void setToken(String value) { this.token = value; }
+        public void setType(long value) {
+            this.type = value;
+        }
 
-        public String getGuildLocale() { return guildLocale; }
-        public void setGuildLocale(String value) { this.guildLocale = value; }
+        public String getLocale() {
+            return locale;
+        }
+
+        public void setLocale(String value) {
+            this.locale = value;
+        }
+
+        public long getVersion() {
+            return version;
+        }
+
+        public void setVersion(long value) {
+            this.version = value;
+        }
+
+        public String getChannelid() {
+            return channelid;
+        }
+
+        public void setChannelid(String value) {
+            this.channelid = value;
+        }
+
+        public String getApplicationid() {
+            return applicationid;
+        }
+
+        public void setApplicationid(String value) {
+            this.applicationid = value;
+        }
+
+        public String getToken() {
+            return token;
+        }
+
+        public void setToken(String value) {
+            this.token = value;
+        }
+
+        public String getGuildLocale() {
+            return guildLocale;
+        }
+
+        public void setGuildLocale(String value) {
+            this.guildLocale = value;
+        }
     }
 
     public static class Data {
@@ -76,17 +167,37 @@ public class Interaction {
         private String id;
         private long type;
 
-        public String getName() { return name; }
-        public void setName(String value) { this.name = value; }
+        public String getName() {
+            return name;
+        }
 
-        public String getGuildid() { return guildid; }
-        public void setGuildid(String value) { this.guildid = value; }
+        public void setName(String value) {
+            this.name = value;
+        }
 
-        public String getid() { return id; }
-        public void setid(String value) { this.id = value; }
+        public String getGuildid() {
+            return guildid;
+        }
 
-        public long getType() { return type; }
-        public void setType(long value) { this.type = value; }
+        public void setGuildid(String value) {
+            this.guildid = value;
+        }
+
+        public String getid() {
+            return id;
+        }
+
+        public void setid(String value) {
+            this.id = value;
+        }
+
+        public long getType() {
+            return type;
+        }
+
+        public void setType(long value) {
+            this.type = value;
+        }
     }
 
     public static class Member {
@@ -100,32 +211,77 @@ public class Interaction {
         private User user;
         private boolean isPending;
 
-        public List<String> getRoles() { return roles; }
-        public void setRoles(List<String> value) { this.roles = value; }
+        public List<String> getRoles() {
+            return roles;
+        }
 
-        public boolean getPending() { return pending; }
-        public void setPending(boolean value) { this.pending = value; }
+        public void setRoles(List<String> value) {
+            this.roles = value;
+        }
 
-        public long getFlags() { return flags; }
-        public void setFlags(long value) { this.flags = value; }
+        public boolean getPending() {
+            return pending;
+        }
 
-        public boolean getDeaf() { return deaf; }
-        public void setDeaf(boolean value) { this.deaf = value; }
+        public void setPending(boolean value) {
+            this.pending = value;
+        }
 
-        public boolean getMute() { return mute; }
-        public void setMute(boolean value) { this.mute = value; }
+        public long getFlags() {
+            return flags;
+        }
 
-        public OffsetDateTime getJoinedAt() { return joinedAt; }
-        public void setJoinedAt(OffsetDateTime value) { this.joinedAt = value; }
+        public void setFlags(long value) {
+            this.flags = value;
+        }
 
-        public String getPermissions() { return permissions; }
-        public void setPermissions(String value) { this.permissions = value; }
+        public boolean getDeaf() {
+            return deaf;
+        }
 
-        public User getUser() { return user; }
-        public void setUser(User value) { this.user = value; }
+        public void setDeaf(boolean value) {
+            this.deaf = value;
+        }
 
-        public boolean getIsPending() { return isPending; }
-        public void setIsPending(boolean value) { this.isPending = value; }
+        public boolean getMute() {
+            return mute;
+        }
+
+        public void setMute(boolean value) {
+            this.mute = value;
+        }
+
+        public OffsetDateTime getJoinedAt() {
+            return joinedAt;
+        }
+
+        public void setJoinedAt(OffsetDateTime value) {
+            this.joinedAt = value;
+        }
+
+        public String getPermissions() {
+            return permissions;
+        }
+
+        public void setPermissions(String value) {
+            this.permissions = value;
+        }
+
+        public User getUser() {
+            return user;
+        }
+
+        public void setUser(User value) {
+            this.user = value;
+        }
+
+        public boolean getIsPending() {
+            return isPending;
+        }
+
+        public void setIsPending(boolean value) {
+            this.isPending = value;
+        }
     }
 
     public static class User {
@@ -135,19 +291,44 @@ public class Interaction {
         private String username;
         private String discriminator;
 
-        public long getPublicFlags() { return publicFlags; }
-        public void setPublicFlags(long value) { this.publicFlags = value; }
+        public long getPublicFlags() {
+            return publicFlags;
+        }
 
-        public String getid() { return id; }
-        public void setid(String value) { this.id = value; }
+        public void setPublicFlags(long value) {
+            this.publicFlags = value;
+        }
 
-        public String getAvatar() { return avatar; }
-        public void setAvatar(String value) { this.avatar = value; }
+        public String getid() {
+            return id;
+        }
 
-        public String getUsername() { return username; }
-        public void setUsername(String value) { this.username = value; }
+        public void setid(String value) {
+            this.id = value;
+        }
 
-        public String getDiscriminator() { return discriminator; }
-        public void setDiscriminator(String value) { this.discriminator = value; }
+        public String getAvatar() {
+            return avatar;
+        }
+
+        public void setAvatar(String value) {
+            this.avatar = value;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String value) {
+            this.username = value;
+        }
+
+        public String getDiscriminator() {
+            return discriminator;
+        }
+
+        public void setDiscriminator(String value) {
+            this.discriminator = value;
+        }
     }
 }
--- src/main/java/io/github/realyusufismail/websocket/json/Payload.java
+++ src/main/java/io/github/realyusufismail/websocket/json/Payload.java
@@ -1,5 +1,21 @@
-//YApi QuickType插件生成，具体参考文档:https://github.com/RmondJone/YapiQuickType
-
+/*
+ * Copyright 2022 Yusuf Arfan Ismail and other YDW contributors.
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
 package io.github.realyusufismail.websocket.json;
 
 import java.util.List;
@@ -10,17 +26,37 @@ public class Payload {
     private T t;
     private D d;
 
-    public Op getOp() { return op; }
-    public void setOp(Op value) { this.op = value; }
+    public Op getOp() {
+        return op;
+    }
 
-    public Op getS() { return s; }
-    public void setS(Op value) { this.s = value; }
+    public void setOp(Op value) {
+        this.op = value;
+    }
 
-    public T getT() { return t; }
-    public void setT(T value) { this.t = value; }
+    public Op getS() {
+        return s;
+    }
 
-    public D getD() { return d; }
-    public void setD(D value) { this.d = value; }
+    public void setS(Op value) {
+        this.s = value;
+    }
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T value) {
+        this.t = value;
+    }
+
+    public D getD() {
+        return d;
+    }
+
+    public void setD(D value) {
+        this.d = value;
+    }
 
     public static class D {
         private UserSettings userSettings;
@@ -36,77 +72,167 @@ public class Payload {
         private List<Object> privateChannels;
         private User user;
 
-        public UserSettings getUserSettings() { return userSettings; }
-        public void setUserSettings(UserSettings value) { this.userSettings = value; }
+        public UserSettings getUserSettings() {
+            return userSettings;
+        }
 
-        public List<Object> getRelationships() { return relationships; }
-        public void setRelationships(List<Object> value) { this.relationships = value; }
+        public void setUserSettings(UserSettings value) {
+            this.userSettings = value;
+        }
 
-        public List<T> getTrace() { return trace; }
-        public void setTrace(List<T> value) { this.trace = value; }
+        public List<Object> getRelationships() {
+            return relationships;
+        }
 
-        public List<Object> getPresences() { return presences; }
-        public void setPresences(List<Object> value) { this.presences = value; }
+        public void setRelationships(List<Object> value) {
+            this.relationships = value;
+        }
 
-        public Application getApplication() { return application; }
-        public void setApplication(Application value) { this.application = value; }
+        public List<T> getTrace() {
+            return trace;
+        }
 
-        public List<Guild> getGuilds() { return guilds; }
-        public void setGuilds(List<Guild> value) { this.guilds = value; }
+        public void setTrace(List<T> value) {
+            this.trace = value;
+        }
 
-        public Op getV() { return v; }
-        public void setV(Op value) { this.v = value; }
+        public List<Object> getPresences() {
+            return presences;
+        }
 
-        public List<Object> getGuildJoinRequests() { return guildJoinRequests; }
-        public void setGuildJoinRequests(List<Object> value) { this.guildJoinRequests = value; }
+        public void setPresences(List<Object> value) {
+            this.presences = value;
+        }
 
-        public List<T> getGeoOrderedrtcRegions() { return geoOrderedrtcRegions; }
-        public void setGeoOrderedrtcRegions(List<T> value) { this.geoOrderedrtcRegions = value; }
+        public Application getApplication() {
+            return application;
+        }
 
-        public T getSessionid() { return sessionid; }
-        public void setSessionid(T value) { this.sessionid = value; }
+        public void setApplication(Application value) {
+            this.application = value;
+        }
 
-        public List<Object> getPrivateChannels() { return privateChannels; }
-        public void setPrivateChannels(List<Object> value) { this.privateChannels = value; }
+        public List<Guild> getGuilds() {
+            return guilds;
+        }
 
-        public User getUser() { return user; }
-        public void setUser(User value) { this.user = value; }
+        public void setGuilds(List<Guild> value) {
+            this.guilds = value;
+        }
+
+        public Op getV() {
+            return v;
+        }
+
+        public void setV(Op value) {
+            this.v = value;
+        }
+
+        public List<Object> getGuildJoinRequests() {
+            return guildJoinRequests;
+        }
+
+        public void setGuildJoinRequests(List<Object> value) {
+            this.guildJoinRequests = value;
+        }
+
+        public List<T> getGeoOrderedrtcRegions() {
+            return geoOrderedrtcRegions;
+        }
+
+        public void setGeoOrderedrtcRegions(List<T> value) {
+            this.geoOrderedrtcRegions = value;
+        }
+
+        public T getSessionid() {
+            return sessionid;
+        }
+
+        public void setSessionid(T value) {
+            this.sessionid = value;
+        }
+
+        public List<Object> getPrivateChannels() {
+            return privateChannels;
+        }
+
+        public void setPrivateChannels(List<Object> value) {
+            this.privateChannels = value;
+        }
+
+        public User getUser() {
+            return user;
+        }
+
+        public void setUser(User value) {
+            this.user = value;
+        }
     }
 
     public static class Application {
         private Op flags;
         private T id;
 
-        public Op getFlags() { return flags; }
-        public void setFlags(Op value) { this.flags = value; }
+        public Op getFlags() {
+            return flags;
+        }
 
-        public T getid() { return id; }
-        public void setid(T value) { this.id = value; }
+        public void setFlags(Op value) {
+            this.flags = value;
+        }
+
+        public T getid() {
+            return id;
+        }
+
+        public void setid(T value) {
+            this.id = value;
+        }
     }
 
     public static class Op {
         private long num;
 
-        public long getNum() { return num; }
-        public void setNum(long value) { this.num = value; }
+        public long getNum() {
+            return num;
+        }
+
+        public void setNum(long value) {
+            this.num = value;
+        }
     }
 
     public static class T {
         private String value;
 
-        public String getValue() { return value; }
-        public void setValue(String value) { this.value = value; }
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
     }
 
     public static class Guild {
         private UserSettings unavailable;
         private T id;
 
-        public UserSettings getUnavailable() { return unavailable; }
-        public void setUnavailable(UserSettings value) { this.unavailable = value; }
+        public UserSettings getUnavailable() {
+            return unavailable;
+        }
 
-        public T getid() { return id; }
-        public void setid(T value) { this.id = value; }
+        public void setUnavailable(UserSettings value) {
+            this.unavailable = value;
+        }
+
+        public T getid() {
+            return id;
+        }
+
+        public void setid(T value) {
+            this.id = value;
+        }
     }
 
     public static class UserSettings {
@@ -123,31 +249,76 @@ public class Payload {
         private T username;
         private T discriminator;
 
-        public UserSettings getBot() { return bot; }
-        public void setBot(UserSettings value) { this.bot = value; }
+        public UserSettings getBot() {
+            return bot;
+        }
 
-        public UserSettings getVerified() { return verified; }
-        public void setVerified(UserSettings value) { this.verified = value; }
+        public void setBot(UserSettings value) {
+            this.bot = value;
+        }
 
-        public Op getFlags() { return flags; }
-        public void setFlags(Op value) { this.flags = value; }
+        public UserSettings getVerified() {
+            return verified;
+        }
 
-        public UserSettings getMfaEnabled() { return mfaEnabled; }
-        public void setMfaEnabled(UserSettings value) { this.mfaEnabled = value; }
+        public void setVerified(UserSettings value) {
+            this.verified = value;
+        }
 
-        public T getid() { return id; }
-        public void setid(T value) { this.id = value; }
+        public Op getFlags() {
+            return flags;
+        }
 
-        public UserSettings getAvatar() { return avatar; }
-        public void setAvatar(UserSettings value) { this.avatar = value; }
+        public void setFlags(Op value) {
+            this.flags = value;
+        }
 
-        public UserSettings getEmail() { return email; }
-        public void setEmail(UserSettings value) { this.email = value; }
+        public UserSettings getMfaEnabled() {
+            return mfaEnabled;
+        }
 
-        public T getUsername() { return username; }
-        public void setUsername(T value) { this.username = value; }
+        public void setMfaEnabled(UserSettings value) {
+            this.mfaEnabled = value;
+        }
 
-        public T getDiscriminator() { return discriminator; }
-        public void setDiscriminator(T value) { this.discriminator = value; }
+        public T getid() {
+            return id;
+        }
+
+        public void setid(T value) {
+            this.id = value;
+        }
+
+        public UserSettings getAvatar() {
+            return avatar;
+        }
+
+        public void setAvatar(UserSettings value) {
+            this.avatar = value;
+        }
+
+        public UserSettings getEmail() {
+            return email;
+        }
+
+        public void setEmail(UserSettings value) {
+            this.email = value;
+        }
+
+        public T getUsername() {
+            return username;
+        }
+
+        public void setUsername(T value) {
+            this.username = value;
+        }
+
+        public T getDiscriminator() {
+            return discriminator;
+        }
+
+        public void setDiscriminator(T value) {
+            this.discriminator = value;
+        }
     }
 }
